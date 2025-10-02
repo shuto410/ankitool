68f38cd387241ac7f78f17904bba57e8539e4cdf
Takumi Shuto <takumi.shuto@rakuten.com>
2025-10-02 19:55:51 +0900

    Add database integration and UI updates for word display

--- App.tsx
+++ App.tsx
@@ -5,12 +5,22 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
+import { database, User, Word } from './database';
+import { useEffect, useState } from 'react';
+// database.init();
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -26,9 +36,41 @@ function App() {
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
+  const [words, setWords] = useState<Word[]>([]);
+  const [tables, setTables] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log('useEffect');
+    const loadWords = async () => {
+      console.log('loadWords');
+      await database.init();
+      console.log('database.init');
+
+      // Get all tables first
+      const allTables = database.getAllTables();
+      setTables(allTables);
+      console.log('All tables:', allTables);
+
+      const words = await database.getAllWords();
+      if (words.length !== 0) {
+        console.log('words', words);
+        setWords(words);
+      } else {
+        console.log('no words');
+      }
+    };
+    loadWords();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={{ paddingTop: safeAreaInsets.top }}>Hello World</Text>
+      <Text style={{ paddingTop: safeAreaInsets.top }}>Anki Words</Text>
+      <Text>Tables in database: {tables.join(', ')}</Text>
+      {words.map(word => (
+        <Text key={word.item_id}>
+          {word.word} - {word.mean}
+        </Text>
+      ))}
     </View>
   );
 }
